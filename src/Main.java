b3c2c73cf9dbd71dcc1734f83786dbcd2b8d03a0
Andreydenisov7788 <asasa88881974@mail.ru>
2022-11-02 03:21:56 +0300

    ДЗ Автомобиль

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        Human human1 = new Human(null, "Минск", 1988,"бренд-менеджер");
-        Human human2 = new Human("Аня", " Москва", 1993,"методист образовательных программ");
-        Human human3 = new Human("Катя", "Калининград", 1992, "продакт-менеджер");
-        Human human4 = new Human("Артем", "Москва", 1995, "директора по развитию бизнеса");
-        System.out.println(human1);
-        System.out.println(human2);
-        System.out.println(human3);
-        System.out.println(human4);
-
-
+        Car car1 = new Car("Лада", "Гранта", 2015, "Россия", null,
+                0);
+        Car car2 = new Car("Audi ", "A8 50 L TDI quattro", 0 , "Германии", "черный ",
+                3.0 );
+        Car car3 = new Car("BMW ", "Z8", 2021 , "Германии ", "черный ",
+                3.0);
+        Car car4 = new Car("Kia ", " Sportage 4-го поколения", 2018 , " Южной Корее", "красный",
+                2.4 );
+        Car car5 = new Car("Hyundai ", "Avante", 2016 , "Южной Корее", "оранжевый",
+                1.6 );
+        System.out.println(car1);
+        System.out.println(car2);
+        System.out.println(car3);
+        System.out.println(car4);
+        System.out.println(car5);
 
     }
 }
